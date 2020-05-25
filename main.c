d076ab34a9e800272ccd7c63f986d8af766e7720
talash ganvir <talashganvir08@gmail.com>
2020-05-25 09:45:46 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * Author: talash.ganvir
+ *
+ * Created on 26 July, 2019, 4:26 PM
+ */
+
+
+#include <xc.h>
+
+void main(void)
+{
+    return;
+}
---
+++ newmain.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   newmain.c
+ * Author: talash.ganvir
+ *
+ * Created on 26 July, 2019, 4:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
